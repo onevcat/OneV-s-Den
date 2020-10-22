fda7a142f1afc5fcfa9d3d19f8acb2e404b090bb
onevcat <onevcat@gmail.com>
2020-10-22 09:32:55 +0900

    Remove GA

--- assets/js/_home-post.js
+++ assets/js/_home-post.js
@@ -10,13 +10,3 @@
 
 {% include_relative _utils/timeago.js %}
 
-
-{% if site.google_analytics.pv.enabled %}
-
-  {% include_relative _pv-config.js %}
-
-  {% include_relative _utils/pageviews.js %}
-
-  {% include_relative lib/_countUp.min.js %}
-
-{% endif %}
