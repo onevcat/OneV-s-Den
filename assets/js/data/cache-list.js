58d988118580958c5bc42c211b096641fece4fb1
onevcat <onevcat@gmail.com>
2020-10-22 09:32:55 +0900

    Remove GA

--- assets/js/data/cache-list.js
+++ assets/js/data/cache-list.js
@@ -103,9 +103,6 @@ const include = [
 ];
 
 const exclude = [
-  {%- if site.google_analytics.pv.proxy_url and site.google_analytics.pv.enabled -%}
-    '{{ site.google_analytics.pv.proxy_url }}',
-  {%- endif -%}
   '/assets/js/data/pageviews.json',
   '/img.shields.io/'
 ];
