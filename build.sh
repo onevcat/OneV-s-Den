3d596598ae16d70568d540f2ef5f9711dc39e048
onevcat <onevcat@gmail.com>
2021-01-13 17:41:23 +0900

    Add post

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./tools/build.sh
-rm _site/config.codekit3 _site/build.sh_site/Brewfile _site/Brewfile.lock.json
+rm _site/config.codekit3 _site/build.sh _site/Brewfile _site/Brewfile.lock.json
 rsync -auv _site onevcat@onevcat.com:/home/onevcat/www/blog
